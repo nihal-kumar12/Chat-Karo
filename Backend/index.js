44313da2c4feaa3d0a48d993c7b5572017292ad9
Nihal Kumar <kumarnihal1947@gmail.com>
2024-08-19 04:11:22 +0530

    updatex12314

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:5173/',
   methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
 })); // enable cors for cross-origin requests
 
 const PORT = process.env.PORT || 4001;
